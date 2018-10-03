7107b201f63676ac804bf05a3b068e54fb4fc39d
Eder <edermitten@gmail.com>
2018-10-02 19:25:18 -0500

    Initial Commit

---
+++ Simple Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Player
+//
+//  Created by Eder Mitten on 10/2/18.
+//  Copyright © 2018 Eder Mitten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
